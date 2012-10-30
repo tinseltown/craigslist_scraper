6b7a0c0597c7be24ce0a451d7e8344ce8d617667
Apprentice <apprentice@dbc22.local>
2012-10-29 20:28:51 -0700

    Confirmed SearchResult working

---
+++ lib/search_result.rb
@@ -0,0 +1,25 @@
+module CLScraper
+
+  class SearchResult
+    attr_reader :postings, :created_at
+
+    def initialize
+      @postings = []
+      @created_at = Time.now
+    end
+
+    def self.from_query(result_data_file)
+      result = self.new
+      Nokogiri::HTML(result_data_file).css('.row').each { |row| result.add_posting(row) }
+      result
+    end
+
+    def add_posting(row)
+      @postings << Posting.from_row_data(row)
+    end
+  end
+
+end
+
+class Posting
+end
