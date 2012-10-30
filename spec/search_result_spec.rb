15ad39381258df705fbfc56111e2c2dfb38afad3
Apprentice <apprentice@dbc22.local>
2012-10-29 20:02:26 -0700

    Finished SearchResult class

--- spec/search_result_spec.rb
+++ spec/search_result_spec.rb
@@ -1,10 +1,27 @@
 require_relative '../search_result'
 require 'nokogiri'
+require 'vcr'
+require 'open-uri'
 require 'fakeweb'
+
 include CLScraper
 
 describe SearchResult do
+  before(:each) do
+    VCR.configure do |c|
+      c.cassette_library_dir = 'fixtures/vcr_cassettes'
+      c.hook_into :fakeweb
+    end
+
+    VCR.use_cassette('craigslist') do
+      @response = open("http://sfbay.craigslist.org/search/?areaID=1&subAreaID=&query=macbook+pro&catAbb=sss")
+    end
+
+    Posting.stub(:from_row_data).and_return(true)
+  end
+
   let(:result) { SearchResult.new }
+
   context "#initialize" do
     it "has postings" do
       result.postings.should_not be_nil
@@ -16,38 +33,30 @@ describe SearchResult do
   end
 
   context "#add_posting" do
-    before(:each) do
-      Posting.stub(:from_row_data).and_return(true)
-    end
 
     it "calls on Posting.from_row_data" do
       Posting.should_receive(:from_row_data)
-      result.add_posting("row")
+      result.add_posting(@response)
     end
 
     it "adds a Posting object to @postings" do
-      expect { result.add_posting("row") }.to change { result.postings.length }.by(1)
+      expect { result.add_posting(@response) }.to change { result.postings.length }.by(1)
     end
   end
 
   context "#self.from_query" do
-    before(:each) do
-      FakeWeb.register_uri(:get, "http://craigslist.com/mysearch", :body => "Posting")
-
-      Nokogiri.stub!(:HTML).and_return(@file)
-    end
 
     it "returns an instance of SearchResult" do
-      SearchResult.from_query("data").should be_an_instance_of(SearchResult)
+      SearchResult.from_query(@response).should be_an_instance_of(SearchResult)
     end
 
     it "able to get data from the query" do
-      SearchResult.from_query("data").should_not be_nil
+      SearchResult.from_query(@response).should_not be_nil
     end
 
-    it "parses the data to get the posting rows"
-
-    it "passes each row to add_posting"
+    it "parses the data to get the posting rows" do
+      SearchResult.from_query(@response)
+    end
   end
 
 end
