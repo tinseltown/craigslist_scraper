b8867dd45723f1858142acbbbdd444971593a1ff
Apprentice <apprentice@dbc23.local>
2012-10-29 18:32:27 -0700

    filled in the Query class (returns a results_data_file for a url_as_query). Tests, too.

---
+++ spec/query_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require 'open-uri'
+require '../lib/query.rb'
+
+module CLScraper
+  describe Query do
+
+# ````query_url = "http://www.yahoo.com"
+    let(:query) { Query.new("http://www.yahoo.com") }
+
+    context 'when the user passes in a valid URL representing a craigslist search' do
+      describe '#initialize(query_as_url)' do
+        it 'returns an instance of Query' do
+          query.should be_an_instance_of(Query)
+        end
+
+        it 'raises an error if an invalid URL is passed to #intialize' do
+          expect { Query.new("hpp:/nogood") }.to raise_error
+        end
+      end
+
+      describe '#results_data' do
+        it 'stores the search results page as a file object' do
+          query.results_data.should_not be nil
+        end
+
+        it 'returns a File object representing the search results' do
+          query.results_data.should have_at_least(1).item
+        end
+      end
+
+      describe '#valid_url?' do
+        it 'returns false for an invalid url' do
+          # invalid_url = "htp:/baddy"
+          query.valid_url?("htp:/baddy").should be false
+        end
+
+        it 'returns true for a valid url' do
+          query.valid_url?("http://google.com").should be true
+        end
+      end
+    end
+  end
+end
